a23f1ef6fb2c4a66db9db1658fa985f182b08100
Hayri Can <can.barcin@scoutium.com>
2021-12-22 14:43:59 +0300

    Added success and error functions for json response

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,9 +5,39 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Return a success JSON response.
+     *
+     * @param  array|string  $data
+     * @param  int|null  $code
+     * @return JsonResponse
+     */
+    protected function success($data, int $code = 200): JsonResponse
+    {
+        return response()->json($data, $code);
+    }
+
+    /**
+     * Return an error JSON response.
+     *
+     * @param  string  $message
+     * @param  int  $code
+     * @param  array|string|null  $exception
+     * @return JsonResponse
+     */
+    protected function error(string $message, int $code, $exception = null): JsonResponse
+    {
+        return response()->json([
+            'error' => true,
+            'message' => $message,
+            'exception' => $exception
+        ], $code);
+    }
 }
